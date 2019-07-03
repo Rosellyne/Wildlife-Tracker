557fc6bc913abe776e5b4eeccfc82b5a54700498
Rosellyne <elijahroselyne@gmail.com>
2019-07-03 12:32:03 +0300

    created the animal class and its test class

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -1,2 +1,29 @@
+import java.util.Objects;
+
 public class Animal {
+    private String name;
+    private int id;
+
+
+    public Animal(String name, int id) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Animal)) return false;
+        Animal animal = (Animal) o;
+        return id == animal.id &&
+                Objects.equals(name, animal.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, id);
+    }
 }
--- src/test/java/AnimalTest.java
+++ src/test/java/AnimalTest.java
@@ -1,5 +1,33 @@
+import org.junit.Test;
+import org.junit.*;
 import static org.junit.Assert.*;
 
 public class AnimalTest {
+    @Test
+    public void animal_instantiatesCorectly_true(){
+        Animal  testAnimal = new Animal("Lion",1);
+        assertEquals(true,testAnimal instanceof Animal);
+    }
+    @Test
+    public void getName_animalinstantiatesWithName_Lion(){
+        Animal  testAnimal = new Animal("Lion",1);
+        assertEquals("Lion",testAnimal.getName());
+
+    }
+    @Test
+    public void returnTrueIfNameAndIdAreSame_true(){
+        Animal  testAnimal = new Animal("Lion",1);
+        Animal  secondAnimal = new Animal("Lion",1);
+        assertTrue(testAnimal.equals(secondAnimal));
+
+    }
+    @Test
+    public void save_insertsObjectsIntoDatabase_Animal(){
+        Animal  testAnimal = new Animal("Lion",1);
+        testAnimal.save();
+        assertEquals(Animal.all().get(0).equals(testAnimal));
+    }
+
+
 
 }
