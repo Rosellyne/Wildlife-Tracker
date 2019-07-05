2dd055d8fb37882debaa386f92becf73e17509a9
Rosellyne <elijahroselyne@gmail.com>
2019-07-05 06:41:14 +0300

    created the  css and added the  styling to h1 and the forms

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,6 +2,7 @@ import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static spark.Spark.staticFileLocation;
@@ -30,6 +31,12 @@ public class App {
             String animalName = request.queryParams("animalName");
             String animalHealth = request.queryParams("animalHealth");
             String userName = request.queryParams("userName");
+            List<Sighting> allSightings = Sighting.all();
+            List<EndangeredAnimal> allAnimal= EndangeredAnimal.all();
+            List<NonEndangeredAnimal> animal=NonEndangeredAnimal.all();
+            model.put("sightings",allSightings);
+            model.put("animal",allAnimal);
+            model.put("animal",animal);
             return new ModelAndView(model,"sighting.hbs");
         },new HandlebarsTemplateEngine());
 
@@ -38,6 +45,22 @@ public class App {
         get ("/animal",(request, response) -> {
             Map<String,Object>model =new HashMap<>();
             return new ModelAndView(model,"animal-form.hbs");
+        },new HandlebarsTemplateEngine());
+
+
+        post("/animal/new",(request,response)->{
+            Map<String, Object> model = new HashMap<>();
+            boolean endangered = request.queryParams("endangered")!=null;
+            if (endangered) {
+                String name = request.queryParams("name");
+//                EndangeredAnimal endangeredAnimal = new EndangeredAnimal;
+//                endangeredAnimal.save();
+            }else{
+                String name = request.queryParams("name");
+
+            }
+
+            return new ModelAndView(model,"sighting.hbs");
         },new HandlebarsTemplateEngine());
     }
 }
